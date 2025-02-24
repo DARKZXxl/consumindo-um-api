f4c1e5df49e6eeb5fda568b7a61e26198e69546d
DARKZXxl <gabrielfigueira88@gmail.com>
2025-02-23 22:55:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+import { criarTr } from "./criarTr.js";
+import { criarTrComImg } from "./criarTrComImg.js";
+import { mudarCorDaCasa } from "./mudarCorDaCasa.js";
+
+async function requisicao() {
+    const resposta = await fetch('https://hp-api.onrender.com/api/characters').then(res => res.json())
+    console.log(resposta)
+
+    resposta.forEach(personagem => {
+        mostrarNaTela(personagem)
+    });
+}
+
+function mostrarNaTela(data) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    
+    const img = document.createElement('img')
+    img.src = data.image
+    
+    const table = document.createElement('table')
+
+    const name = document.createElement('td')
+    name.textContent = data.name
+    name.classList.add('name')
+    
+
+    const dateOfBirth = criarTr(data.dateOfBirth, 'date Of Birth')
+    const patronus = criarTr(data.patronus, 'patronus')
+    const gender = criarTrComImg(data.gender ,'gender', 'https://img.icons8.com/color/48/male.png',  'https://img.icons8.com/color/48/female.png')
+    const species = criarTr(data.species, 'species')
+    const house = criarTr(data.house, 'house')
+    const wizard = criarTr(data.wizard, 'wizard')    
+    const actor = criarTr(data.actor, 'actor')
+    const ancestry = criarTr(data.ancestry, 'ancestry')
+
+    table.append(species, gender, house, wizard, actor, ancestry, patronus, dateOfBirth)
+    card.append(name, img, table)
+    document.querySelector('main').appendChild(card)
+
+    mudarCorDaCasa()
+}
+
+requisicao()
